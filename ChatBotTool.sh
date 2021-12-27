c55bc1d3311581f05377a9208df01cf886f570d3
ScorpioHack999 <96740598+ScorpioHack999@users.noreply.github.com>
2021-12-27 22:55:44 +0530

    Add files via upload

---
+++ ChatBotTool.sh
@@ -0,0 +1,27 @@
+echo -e "\e[32m/| |\ /| |\ /| |\ /| |\ / | | \ / | | \ | | | | | | | | \ \/ / __ __ \ \/ / \ / / / \ \ \ / \ / \ \__/ / \ / \ / / \ \ / _ \ \__/ O O \__/ / _ \\ \___ ___/ // _ \\___/ ______ \___// _ \\ ----( )---- // \\_____( ________ )_____// ~-----( )-----~ _ _____( ________ )_____ \\ /,----( )---- _// // ( ______ ) / \ ~ \ / \ / \ __ / / / \ / / / \ \ / / \ ~----~ / \________/  MrScorpioHack\e[0m";
+echo -e "\e[32mScript Maker : ModsBinYT\e[0m";
+echo -e "\e[31mChat Bot v1.1\e[0m";
+if [ "${userinput0:-}" = "Hi Scorpio" ]
+then
+	echo -e "\e[34mHi I Am Scorpio Welcome To Scorpio Hacks\e[0m";
+fi
+if [ "${userinput0:-}" = "How Are You Scorpio" ]
+then
+	echo -e "\e[34mI Am Fine I Hope Your Family Members Are Also Fine !!\e[0m";
+fi
+if [ "${userinput0:-}" = "Reverse This Text ("Hey There I Am Scorpio User") " ]
+then
+	echo -e "\e[34m(\"Hey There I Am Scorpio User\")  Reversed Text Is (\"User Scorpio Am I There Hey\")\e[0m";
+fi
+if [ "${userinput0:-}" = "./clear" ]
+then
+	clear
+fi
+if [ "${userinput0:-}" = "date" ]
+then
+	date
+fi
+if [ "${userinput0:-}" = "./Exit" ]
+then
+	killall kill
+fi
